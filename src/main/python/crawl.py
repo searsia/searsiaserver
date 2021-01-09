6f23b0834fcfe9b2bb2aa64152ead090d9e3e991
Djoerd Hiemstra <djoerd@searsia.org>
2021-01-09 16:12:30 +0100

    Adds more error handling

--- src/main/python/crawl.py
+++ src/main/python/crawl.py
@@ -1,10 +1,12 @@
 
 import json
+import math
 import sys
 import time
 import re
 import requests
 
+SLEEP = 1
 
 if len(sys.argv) != 2:
     print('Usage: crawl.py <searsia-template>', file=sys.stderr)
@@ -23,7 +25,7 @@ def do_request(template, engine='index', page=1):
     url = re.sub(r'{[^}]+}', '', url)
     try:
         response = requests.get(url)
-        time.sleep(1)
+        time.sleep(SLEEP)
         return response.json()
     except Exception as exception:
         print(exception, file=sys.stderr)
@@ -41,9 +43,14 @@ print('Crawling... please, wait.')
 for page in range(99):
     new_found = False
     result = do_request(template, 'index', page + 1)
+    print('Page', page + 1)
     if 'hits' in result:
         for hit in result['hits']:
             if 'rid' in hit:
+                if hit in hits:
+                    print('Warning: duplicate. Check template startPage.',
+                          file=sys.stderr)
+                    break
                 hits.append(hit)
                 new_found = True
     if not new_found:
@@ -54,13 +61,14 @@ save_engine('index', result)
 print('Ok: index')
 
 if not hits:
-    print('No hits found.', file=sys.stderr)
+    print('Error: No hits found.', file=sys.stderr)
     sys.exit()
 
 for hit in hits:
     engine = hit['rid']
     result = do_request(template, engine)
     save_engine(engine, result)
+    uptime = math.nan
     if 'health' in result:
         health = result['health']
         if ('lastsuccess' in health and 'lasterror' in health and
@@ -71,7 +79,7 @@ for hit in hits:
         if 'requestserr' in health and 'requestsok' in health:
             requestserr = int(health['requestserr'])
             requestsok = int(health['requestsok'])
-            uptime = 100 * requestsok / (requestsok + requestserr)
+            uptime = 100 * requestsok / (requestsok + requestserr + 0.01)
     else:
         conclusion = 'No health'
     print(conclusion + ': ' + engine + ', ' + '{:2.1f}'.format(uptime) + '% uptime')
